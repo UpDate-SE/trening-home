065224b229c124af1367d22bb2ad1232e9af5ad6
luis <47650944+Luisill0@users.noreply.github.com>
2023-06-15 17:50:14 -0600

    components index

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,6 @@
 export { default as BrandsComponent } from './BrandsComponent';
 export { default as CarouselAbout } from './CarouselAbout';
+export { default as CarouselFacilities } from './CarouselFacilities';
 export { default as ContactForm } from './ContactForm';
 export { default as CoursesScrollContainer } from './CoursesScrollContainer';
 export { default as NavbarHome } from './NavbarHome';
