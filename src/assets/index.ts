b2136c9806e045919a4112af5f635b992368e492
luis <47650944+Luisill0@users.noreply.github.com>
2023-06-15 17:51:43 -0600

    assets index

---
+++ src/assets/index.ts
@@ -0,0 +1,7 @@
+export { default as AboutUs } from './about-us.webp';
+export { default as Banner } from './banner.webp';
+export { default as CollegePrep } from './college-prep.webp';
+export { default as Forklift } from './forklift.webp';
+export { default as ShowingInfo } from './showing-info.webp';
+export { default as TransportPersonnel } from './transport-personnel.webp';
+export { default as TreningLogoSm } from './trening-sm.webp';
